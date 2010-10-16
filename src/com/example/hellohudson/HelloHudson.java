b1560a4b383700ccdcb117891b4d490a2b58137f
bols <bols-blue@lnc.jp>
2010-10-16 12:54:19 +0900

    start project add files

---
+++ src/com/example/hellohudson/HelloHudson.java
@@ -0,0 +1,15 @@
+package com.example.hellohudson;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloHudson extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
